514815c06c30a4108ffb523a8141ac8b14ed6eb2
Jorgenoq0193 <orlandoco.ti22@utsjr.edu.mx>
2025-02-05 17:09:34 -0600

    First comit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+cambiarValor() {
+throw new Error('Method not implemented.');
+}
+email: any;
+nombre: any;
+desactivarBoton: any;
+estaActivo() {
+throw new Error('Method not implemented.');
+}
+sumarAlumnos() {
+throw new Error('Method not implemented.');
+}
+contador: any;
+numero: any;
+productos: any;
+aniosServicio: any;
+decrementarContador() {
+throw new Error('Method not implemented.');
+}
+incrementarContador() {
+throw new Error('Method not implemented.');
+}
+  title = 'ejemplo1';
+}
